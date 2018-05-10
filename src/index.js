6c6b12cdaa9e06d85a2a93e3ca0dafe85ba14523
sergey <sy_chugunov@bingo-boom.ru>
2018-05-10 11:58:35 +0300

    - Eslint fixes.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import { debug } from "util";
-
 /* ДЗ 4 - работа с DOM */
 
 /*
@@ -13,7 +11,8 @@ import { debug } from "util";
    createDivWithText('loftschool') // создаст элемент div, поместит в него 'loftschool' и вернет созданный элемент
  */
 function createDivWithText(text) {
-    let divElement = document.createElement("div");
+    let divElement = document.createElement('div');
+
     divElement.textContent = text;
 
     return divElement;
@@ -25,7 +24,8 @@ function createDivWithText(text) {
  Функция должна вставлять элемент, переданный в переметре what в начало элемента, переданного в параметре where
 
  Пример:
-   prepend(document.querySelector('#one'), document.querySelector('#two')) // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
+   prepend(document.querySelector('#one'), document.querySelector('#two')) 
+   // добавит элемент переданный первым аргументом в начало элемента переданного вторым аргументом
  */
 function prepend(what, where) {
     where.insertBefore(what, where.firstChild);
@@ -36,7 +36,8 @@ function prepend(what, where) {
 
  3.1: Функция должна перебрать все дочерние элементы узла, переданного в параметре where
 
- 3.2: Функция должна вернуть массив, состоящий из тех дочерних элементов следующим соседом которых является элемент с тегом P
+ 3.2: Функция должна вернуть массив, состоящий из тех дочерних элементов следующим 
+ соседом которых является элемент с тегом P
 
  Пример:
    Представим, что есть разметка:
@@ -48,13 +49,14 @@ function prepend(what, where) {
       <p></p>
    </dody>
 
-   findAllPSiblings(document.body) // функция должна вернуть массив с элементами div и span т.к. следующим соседом этих элементов является элемент с тегом P
+   findAllPSiblings(document.body) // функция должна вернуть массив с элементами div и span 
+   т.к. следующим соседом этих элементов является элемент с тегом P
  */
 function findAllPSiblings(where) {
     let result = [];
 
     for (let element of where.childNodes) {
-        if (element.nextSibling && element.nextSibling.localName === "p") {
+        if (element.nextSibling && element.nextSibling.localName === 'p') {
             result.push(element);
         }
     }
@@ -65,7 +67,8 @@ function findAllPSiblings(where) {
 /*
  Задание 4:
 
- Функция представленная ниже, перебирает все дочерние узлы типа "элемент" внутри узла переданного в параметре where и возвращает массив из текстового содержимого найденных элементов
+ Функция представленная ниже, перебирает все дочерние узлы типа 'элемент' внутри узла,
+  переданного в параметре where и возвращает массив из текстового содержимого найденных элементов
  Но похоже, что в код функции закралась ошибка и она работает не так, как описано.
 
  Необходимо найти и исправить ошибку в коде так, чтобы функция работала так, как описано выше.
@@ -112,7 +115,8 @@ function deleteTextNodes(where) {
 /*
  Задание 6:
 
- Выполнить предудыщее задание с использование рекурсии - то есть необходимо заходить внутрь каждого дочернего элемента (углубляться в дерево)
+ Выполнить предудыщее задание с использование рекурсии - то есть необходимо заходить внутрь
+  каждого дочернего элемента (углубляться в дерево)
 
  Задачу необходимо решить без использования рекурсии, то есть можно не уходить вглубь дерева.
  Так же будьте внимательны при удалении узлов, т.к. можно получить неожиданное поведение при переборе узлов
@@ -149,20 +153,16 @@ function deleteTextNodesRecursive(where) {
  Постарайтесь не создавать глобальных переменных
 
  Пример:
-   Для дерева <div class="some-class-1"><b>привет!</b> <b class="some-class-1 some-class-2">loftschool</b></div>
+   Для дерева <div class='some-class-1'><b>привет!</b> <b class='some-class-1 some-class-2'>loftschool</b></div>
    должен быть возвращен такой объект:
    {
      tags: { DIV: 1, B: 2},
-     classes: { "some-class-1": 2, "some-class-2": 1 },
+     classes: { 'some-class-1': 2, 'some-class-2': 1 },
      texts: 3
    }
  */
 function collectDOMStat(root) {
     function textNodeCount(element, count) {
-        if (!count) {
-            count = 0;
-        }
-
         let nodes = element.childNodes;
 
         for (let node of nodes) {
@@ -177,10 +177,6 @@ function collectDOMStat(root) {
     }
 
     function classesCount(element, classesObj) {
-        if (!classesObj) {
-            classesObj = {};
-        }
-
         let nodes = element.childNodes;
 
         for (let node of nodes) {
@@ -203,11 +199,7 @@ function collectDOMStat(root) {
         return classesObj;
     }
 
-    function tagsCount(element, tagsObj, skipElement) {
-        if (!tagsObj) {
-            tagsObj = {};
-        }
-
+    function tagsCount(element, tagsObj, skipElement = false) {
         if (!skipElement) {
             if (element.nodeName in tagsObj) {
                 tagsObj[element.nodeName]++;
@@ -219,7 +211,7 @@ function collectDOMStat(root) {
         let nodes = element.children;
 
         for (let node of nodes) {
-            tagsCount(node, tagsObj, false);
+            tagsCount(node, tagsObj);
         }
 
         return tagsObj;
@@ -227,8 +219,8 @@ function collectDOMStat(root) {
 
     let result = {};
 
-    result.texts = textNodeCount(root);
-    result.classes = classesCount(root);
+    result.texts = textNodeCount(root, 0);
+    result.classes = classesCount(root, {});
     result.tags = tagsCount(root, {}, true);
 
     return result;
@@ -267,6 +259,7 @@ function collectDOMStat(root) {
    }
  */
 function observeChildNodes(where, fn) {
+    return fn(where);
 }
 
 export {
